ea2be9ff490f5d0160c20bc5a1ee25253704db46
lensabillion <56606537+lensabillion@users.noreply.github.com>
2020-09-02 12:27:51 +0300

    Create Cycle Detection.py

---
+++ Linked-List/Cycle Detection.py
@@ -0,0 +1,62 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the has_cycle function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def has_cycle(head):
+    if head == None or head.next == None:
+        return False
+    slow = head
+    fast = head.next
+    while slow != fast:
+        
+        if fast == None or fast.next == None:
+            return False
+        fast = fast.next.next
+        slow = slow.next
+    return True
+
+if __name__ == '__main__':
